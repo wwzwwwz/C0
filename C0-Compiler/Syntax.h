c2df73224ebc215b5da363db6da4717cc05185f5
wwzwwwz <474864975@qq.com>
2020-11-06 17:30:12 +0800

    编译器结构

---
+++ C0-Compiler/Syntax.h
@@ -0,0 +1,22 @@
+//语义分析 Syntax
+
+
+
+class Syntax
+{
+private:
+    /* data */
+public:
+    Syntax(/* args */);
+    ~Syntax();
+};
+
+Syntax::Syntax(/* args */)
+{
+}
+
+Syntax::~Syntax()
+{
+}
+
+
